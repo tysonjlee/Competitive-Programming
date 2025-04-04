d951ce16db4a524fefc46ff25dc00e3919efd33d
Tyson <tjjlee@ucdavis.edu>
2025-04-04 10:11:58 -0700

    Modified template.cppw

--- template.cpp
+++ template.cpp
@@ -1,9 +1,10 @@
 //! This template isn't finished yet. 
 
 // Libraries
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> // TODO: Make decision to switch to GCC for this to work or to simply incldue all header files
 using namespace std;
 
+// Overhead
 // TODO: Delete unnecessary 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
@@ -16,7 +17,7 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #endif
 
 // Definitions 
-#define int long long
+// #define int long long
 #define ll long long
 #define ld long double
 #define sza(x) ((int)x.size())
