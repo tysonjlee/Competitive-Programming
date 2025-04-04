9ff833370e812891b0af3f2be96ede49a265c0d8
Tyson <tjjlee@ucdavis.edu>
2025-04-03 17:27:12 -0700

    Modified template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,10 @@
-//! This template is not done yet.
+//! This template isn't finished yet. 
 
 // Libraries
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
+// TODO: Delete unnecessary 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 void dbg_out() {cerr << endl;}
@@ -20,7 +20,7 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #define ll long long
 #define ld long double
 #define sza(x) ((int)x.size())
-#define all(a) (a).begin(), (a).end()
+#define all(a) ((a).begin(), (a).end())
 
 // Constants 
 const int MAX_N = 1e5 + 5;
@@ -38,15 +38,10 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    vector<int> ans; 
-    ans.push_back(2); 
-    ans.push_back(3);
-    for (auto const num : ans) {
-        cout << num; 
-    }
+    // Solve problem 
     int tc = 1;
     // cin >> tc;
     while (tc--) solve(); 
-
+    
     return 0; 
 }
